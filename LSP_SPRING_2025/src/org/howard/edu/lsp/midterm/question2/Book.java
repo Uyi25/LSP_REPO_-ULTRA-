bf6b301ce11b2ff3ad0ff37718e4f6c3642808c1
uyiod <uyiod@Uyi--OO25>
2025-03-20 19:12:04 -0400

    Midterm Assignment 2

--- LSP_SPRING_2025/src/org/howard/edu/lsp/midterm/question2/Book.java
+++ LSP_SPRING_2025/src/org/howard/edu/lsp/midterm/question2/Book.java
@@ -9,7 +9,14 @@ package org.howard.edu.lsp.midterm.question2;
 public class Book {
     private String title, author, ISBN;
     private int yearPublished;
-
+    /**
+     * Constructs a new Book object with the specified details.
+     *
+     * @param title         The title of the book.
+     * @param author        The author of the book.
+     * @param ISBN          The unique ISBN number of the book.
+     * @param yearPublished The year the book was published.
+     */
     public Book(String title, String author, String ISBN, int yearPublished) {
         this.title = title; this.author = author; this.ISBN = ISBN; this.yearPublished = yearPublished;
     }
@@ -26,13 +33,20 @@ public class Book {
         Book book = (Book) obj;
         return ISBN.equals(book.ISBN) && author.equals(book.author);
     }
-
+    /**
+     * Returns a string representation of the book.
+     * The string includes the title, author, ISBN, and year of publication.
+     */
     @Override
     public String toString() {
         return String.format("Title: %s, Author: %s, ISBN: %s, Year Published: %d", title, author, ISBN, yearPublished);
     }
 }
-
+/**
+ * A driver class to test the functionality of the Book class.
+ * This class creates instances of the Book class and demonstrates
+ * the use of its methods, including equality comparison and string representation.
+ */
 class BookDriver {
     public static void main(String[] args) {
         Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "123456789", 1925);
